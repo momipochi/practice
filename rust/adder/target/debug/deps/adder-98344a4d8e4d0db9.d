a6914711be03653d4e8af3951dfbcb33cd73e010
momipochi <k25650727@gmail.com>
2023-12-15 11:54:40 +0100

    commit

---
+++ rust/adder/target/debug/deps/adder-98344a4d8e4d0db9.d
@@ -0,0 +1,5 @@
+C:\code\leet\rust\adder\target\debug\deps\adder-98344a4d8e4d0db9.exe: src\lib.rs
+
+C:\code\leet\rust\adder\target\debug\deps\adder-98344a4d8e4d0db9.d: src\lib.rs
+
+src\lib.rs:
---
+++ rust/adder/target/debug/deps/adder-c6791cac27f307c8.d
@@ -0,0 +1,7 @@
+C:\code\leet\rust\adder\target\debug\deps\adder-c6791cac27f307c8.rmeta: src\lib.rs
+
+C:\code\leet\rust\adder\target\debug\deps\libadder-c6791cac27f307c8.rlib: src\lib.rs
+
+C:\code\leet\rust\adder\target\debug\deps\adder-c6791cac27f307c8.d: src\lib.rs
+
+src\lib.rs:
